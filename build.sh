94597388060142c3b8c1e574337b43f59ae26b4b
bjtj <bjtj10@gmail.com>
2018-03-28 23:36:29 +0900

    initial commit

---
+++ build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+BASE=$PWD
+DIR_BUILD=$PWD/build
+DIR_WORLD=$PWD/world
+
+OPT=build
+
+if [ -f "/proc/cpuinfo" ]; then
+	echo "[OS] linux"
+	CPU_COUNT=$(cat /proc/cpuinfo | grep processor | wc -l)
+elif [ `uname` == "Darwin" ]; then
+	echo "[OS] osx"
+	CPU_COUNT=`sysctl -n hw.ncpu`
+else
+	echo "[OS] unknown"
+	CPU_COUNT=1
+fi
+
+if [ -n "$1" ]; then
+	OPT=$1
+fi
+
+clean() {
+	rm -rf $DIR_BUILD $DIR_WORLD
+}
+
+reconf() {
+	touch NEWS README AUTHORS ChangeLog
+	autoreconf -i
+}
+
+reconf_if_need() {
+	if [ ! -f configure ]; then
+		reconf
+	fi
+}
+
+build() {
+	mkdir -p $DIR_BUILD
+	mkdir -p $DIR_WORLD
+	cd $DIR_BUILD
+	$BASE/configure --prefix="$DIR_WORLD"
+	make -j$CPU_COUNT
+	make install
+}
+
+install() {
+	clean
+	mkdir -p $DIR_BUILD
+	cd $DIR_BUILD
+	$BASE/configure
+	make -j$CPU_COUNT
+	sudo make install
+}
+
+check() {
+	cd $DIR_BUILD
+	make check -j$CPU_COUNT
+}
+
+echo "cpu count := $CPU_COUNT"
+
+case $OPT in
+	reconf)
+		reconf
+		;;
+	build)
+		reconf_if_need
+		build
+		;;
+	all)
+		prepare
+		build
+		;;
+	make)
+		cd $DIR_BUILD && make && make install
+		;;
+	install)
+		reconf_if_need
+		install
+		;;
+	check)
+		check
+		;;
+	dist)
+		cd $DIR_BUILD && make dist
+		;;
+	clean)
+		clean
+		;;
+	*)
+		echo "unknown option - $OPT"
+		exit 1
+		;;
+esac
